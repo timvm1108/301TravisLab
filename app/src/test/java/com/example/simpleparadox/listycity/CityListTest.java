8c32b43544d8e7427b40fec45a620d34354c08d1
Tim van Maren <timvanmaren@hotmail.com>
2020-11-02 00:07:08 -0700

    fixed test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -33,6 +33,6 @@ class CityListTest {
     void testGetCities() {
         CityList cityList = mockCityList();
 
-        assertEquals(1, mockCity().compareTo(cityList.getCities().get(0)));
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0)));
     }
 }
